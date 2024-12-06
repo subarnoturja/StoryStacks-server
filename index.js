d07c82077f64c0225fe16564467e5b478ddcdfcb
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-06 20:34:50 +0600

    delete operation added

--- index.js
+++ index.js
@@ -58,6 +58,14 @@ async function run() {
         res.send(result);
     })
 
+    // Delete Book
+    app.delete('/book/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const result = await booksCollection.deleteOne(filter);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
