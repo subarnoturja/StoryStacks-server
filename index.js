30e3edd9e1adedbd6bebebb07194226ce5efa59d
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-07 03:16:02 +0600

    single book api added

--- index.js
+++ index.js
@@ -69,6 +69,14 @@ async function run() {
         res.send(result);
     })
 
+    // Single Book Data
+    app.get('/book/:id', async(req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const result = await booksCollection.findOne(filter);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
